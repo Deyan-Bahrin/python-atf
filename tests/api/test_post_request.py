c33af4f6dd40cd1df1f132998529cd2a56e24ef5
Deyan Bahrin <deyan.bahrin@1crew.com>
2023-01-25 14:28:52 +0200

    new commit

---
+++ tests/api/test_post_request.py
@@ -0,0 +1,11 @@
+from api_config.api_client import ApiClient
+from api_config.api_request import ApiRequest
+
+
+def test_my_frist_api_test():
+    api_client = ApiClient()
+    request_properties = ApiRequest()\
+        .add_form_data({"grant_type": "client_credentials"})\
+        .add_basic_auth("1v2pem5h5va2c4qcoi3rh4arb4", "vdqp6lputnuq6pgqqr1bevns3vib6c2ir9clj65qo87dfehtu5h").build()
+    response = api_client.post("/oauth2/token", request_properties)
+    print(response.status_code)
---
+++ tests/web/test_first.py
@@ -0,0 +1,16 @@
+from config.browser import Browser
+from config.web_page_factory import WebDriverFactory
+from pages.base_page import BasePage
+from pages.home_page import FtHomePage
+
+
+class Tests:
+
+    base_url = BasePage.init_config()
+    driver = WebDriverFactory.get_driver(Browser.CHROME)
+
+    def test_hello(self):
+        self.driver.get(self.base_url)
+        home_pages = FtHomePage(self.driver)
+        subscribe_page = home_pages.click_on_subscribe_button()
+        subscribe_page.click_on_weekend_button()
