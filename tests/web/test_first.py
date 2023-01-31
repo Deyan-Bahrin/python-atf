c27a092cb8cf37767a89d625e7d5e2ff1709a09b
Deyan-Bahrin <deyan.bahrin@1crew.com>
2023-01-31 19:25:21 +0200

    last commit 31

--- tests/web/test_first.py
+++ tests/web/test_first.py
@@ -11,11 +11,10 @@ class Tests:
     base_url = BasePage.init_config()
     driver = WebDriverFactory.get_driver(Browser.CHROME)
 
+    @allure.id("1")
     @allure.step("test")
     def test_hello(self):
         self.driver.get(self.base_url)
         home_pages = FtHomePage(self.driver)
-        step("Open Page")
         subscribe_page = home_pages.click_on_subscribe_button()
-        step("Check")
         subscribe_page.click_on_weekend_button()
